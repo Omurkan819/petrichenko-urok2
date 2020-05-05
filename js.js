e39b11edda0d5bab7ae7ac34b9b8bd5b009c33ea
Omurkan Abaskanov <oabaskanov@mail.ru>
2020-05-05 09:31:46 +0600

    First Commit

---
+++ js.js
@@ -0,0 +1,37 @@
+// 'use strict';
+
+// if (2 * 4 == 8) {
+//   console.log("Верно!")
+// } else {
+//   console.log("Неверно")
+// }
+
+let num = 50;
+
+// if (num < 49) {
+//   console.log('Неверно!')
+// } else if (num > 100) {
+//   console.log("Много!")
+// } else {
+//   console.log("Верно!")
+// };
+
+// (num == 50) ? console.log("Вверно!"): console.log("Невверно!");
+
+switch (num) {
+  case num < 49:
+    console.log("Неверно!");
+    break;
+  case num > 100:
+    console.log('Много!');
+    break;
+  case num > 80:
+    console.log("Все еще много!");
+    break;
+  case 50:
+    console.log('Верно!');
+    break;
+  default:
+    console.log("Что-то пошло не так!");
+    break;
+}
